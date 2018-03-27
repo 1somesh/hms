4ec0ae36a72bbd11023b278f99acebdca9449d86
somesh <sharmasomesh8448@gmail.com>
2018-03-27 16:28:28 +0530

    index file modified and seperate partials added for doctor and patient

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,7 +3,7 @@ class AppointmentsController < ApplicationController
 
 
   def index
-      @appointment_list = current_user.future_appointment_list.paginate(:page => params[:page], :per_page => 15)
+      @appointment_list = current_user.future_appointment_list.paginate(:page => params[:page], :per_page => 5)
   
   end
 
@@ -25,7 +25,6 @@ class AppointmentsController < ApplicationController
           ExpiredDateWorker.perform_in(@appointment.date + (duration.strftime("%H").to_i + params[:appointment][:start_time].to_i)*60*60,@appointment.id)
           @appointment.create_image(params[:appointment][:image])
           flash[:success] = "Appointment Created!"
-          expire_page '/appointments'
       else
           time = @appointment.date!=nil ? @appointment.date : Time.now+1.day  
           @slots = Appointment.get_booked_slots(@appointment.doctor.id,time.strftime("%Y-%m-%d"))
@@ -36,51 +35,68 @@ class AppointmentsController < ApplicationController
   end
 
   def update
-      @appointment = Appointment.find_by_id params[:id]  
-      authorize! :update, @appointment
-      duration = @appointment.get_appointment_duration(params[:appointment][:start_time])
-      @appointment.create_image(params[:appointment][:image]) 
-      new_date = Appointment.get_new_date(params.require(:appointment).permit(:date))
+      @appointment = Appointment.find_by_id params[:id] 
+       if @appointment.nil?
+        redirect_to root_path
+      else  
+        authorize! :update, @appointment
+        duration = @appointment.get_appointment_duration(params[:appointment][:start_time])
+        @appointment.create_image(params[:appointment][:image]) 
+        new_date = Appointment.get_new_date(params.require(:appointment).permit(:date))
 
-      if params[:appointment][:start_time].nil?
-         if @appointment.date.strftime("%Y-%m-%d").to_date != new_date
-            @appointment.errors.add('appointment','Select a Appointment Time')
-            @slots = Appointment.get_booked_slots(@appointment.doctor.id,@appointment.date)
-            render "edit" and return            
-         end   
-      else
-         @appointment.update_attributes(appointment_update_params)
-         @appointment.update_worker(params[:appointment][:start_time],duration)
-      end
-      flash[:success] = "Appointment Updated!"
-      redirect_to "/appointments" 
+        if params[:appointment][:start_time].nil?
+           if @appointment.date.strftime("%Y-%m-%d").to_date != new_date
+              @appointment.errors.add('appointment','Select a Appointment Time')
+              @slots = Appointment.get_booked_slots(@appointment.doctor.id,@appointment.date)
+              render "edit" and return            
+           end   
+        else
+           @appointment.update_attributes(appointment_update_params)
+           @appointment.update_worker(params[:appointment][:start_time],duration)
+        end
+        flash[:success] = "Appointment Updated!"
+        redirect_to "/appointments" 
+      end  
   end
 
   def destroy
       @appointment = Appointment.find_by_id params[:id]
-      authorize! :destroy, @appointment
-      if @appointment.cancelled!
-          render json: {status: "cancelled"}
-      else
-          render json: {status: "not cancelled"}
+       if @appointment.nil?
+        redirect_to root_path
+      else 
+        authorize! :destroy, @appointment
+        if @appointment.cancelled!
+            render json: {status: "cancelled"}
+        else
+            render json: {status: "not cancelled"}
+        end  
       end    
   end
 
   def show
       @appointment = Appointment.find_by_id params[:id]
-      authorize! :show, @appointment
-      @patient = @appointment.patient
-      @notes = @appointment.notes
+      if @appointment.nil?
+        redirect_to root_path
+      else  
+        authorize! :show, @appointment
+        @patient = @appointment.patient
+        @note = Note.new(user_id: current_user.id,appointment_id: params[:id])
+        @notes = @appointment.notes
+      end  
   end
 
   def edit
       @appointment = Appointment.find_by_id params[:id]
-      authorize! :edit, @appointment    
-      @slots = Appointment.get_booked_slots(@appointment.doctor.id,@appointment.date) 
+       if @appointment.nil?
+        redirect_to root_path
+      else 
+        authorize! :edit, @appointment    
+        @slots = Appointment.get_booked_slots(@appointment.doctor.id,@appointment.date) 
+      end  
   end
 
   def recent
-      @appointments = current_user.past_appointment_list.paginate(:page => params[:page], :per_page => 15)
+      @appointments = current_user.past_appointment_list.paginate(:page => params[:page], :per_page => 5)
   end
 
   def get_available_slots
